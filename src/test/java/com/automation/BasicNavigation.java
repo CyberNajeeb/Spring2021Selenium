93be27746ee757e483525780f2de0658e88ff44c
Najeeb Sohrabi <najeebsohrabi@gmail.com>
2021-04-19 15:24:41 -0700

    Second commit

--- src/test/java/com/automation/BasicNavigation.java
+++ src/test/java/com/automation/BasicNavigation.java
@@ -1,20 +1,27 @@
 package com.automation;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.Assert;
+import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class BasicNavigation {
-    public static void main(String[] args) throws InterruptedException {
-        //WebDriverManager driverManager = new chromedriver();
+
+    @Test
+    public void test1() throws InterruptedException {
+
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
 
         driver.get("http://google.com");
         driver.findElement(By.name("q")).sendKeys("Java Books To Read", Keys.ENTER);
-        Thread.sleep(3000);
+        Thread.sleep(1000);
+        System.out.println(driver.getTitle());
+        Assert.assertEquals("Java Books To Read - Google Search", driver.getTitle());
         driver.quit();
     }
+
 }
